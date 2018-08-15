07d61942373b49e2d35706467b94b263f358c0b1
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 16:25:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,9 @@
 def oxford_comma(array)
-  if array.length == 1 
-    result = array[0]
-  elsif array.length == 2
-    result = array.join(" and ")
-  else array.length > 2 
-    result_init = "#{array[0..-2].join(", ")
-    result_last = array.last.insert(0, "and ")
-    result = "#{result_init},  #{result_last}"
-  end 
-  result
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
-
 
