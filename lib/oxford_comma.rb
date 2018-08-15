81e6182379cf714893c6580ed57368984b5195be
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 16:13:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif array.length > 2 
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")    
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2
+    array.join("and ")
+  else array.length > 2 
+    array.last.insert(0, "and ")
+  end 
+  array.join(", ")
 end
 
 
