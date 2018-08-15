2eb291913313c39cf49dde2c29b5b3b53922caa0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 16:17:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   if array.length == 1 
-    array[0]
+    result = array[0]
   elsif array.length == 2
-    array.join(" and ")
+    result = array.join(" and ")
   else array.length > 2 
-    array.last.insert(0, "and ")
+    result = array.last.insert(0, "and ")
   end 
-  array.join(", ")
+  result
 end
 
 
