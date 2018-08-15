7e6b3d28b4b40a368cd69fb730ad1a694b529cfd
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 15:56:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,5 @@
 def oxford_comma(array)
   if array.length == 1 
     array[0]
-  elsif array.length == 2 
-    array.split(" and ")
-  # elsif array.length == 3
-  #   array[0,1].split(",")+array[2].split("and")
   end 
 end
