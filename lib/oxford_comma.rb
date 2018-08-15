111441b2588ecf2638c1880a4df6b77789c12a65
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 16:12:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
-# def oxford_comma(array)
-#   if array.length == 1 
-#     array[0]
-#   elsif array.length == 2
-#     array.join(" and ")
-#   else array.length > 2 
-#     array.last.insert(0, "and ")
-#   end 
+def oxford_comma(array)
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else array.length > 2 
+    array.last.insert(0, "and ")
+  end 
   array.join(",")
-# end
+end
 
 
